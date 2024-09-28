bab527bab2c31e642bdacb0da47fc6eb1f699e8f
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-09-28 22:26:58 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
-
+import { FaGithub, FaTwitter } from 'react-icons/fa';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { FiMail } from 'react-icons/fi';
 const Footer = () => (
   <footer className="bg-gray-900 text-white p-4">
     <div className="container mx-auto text-center">
       <p>&copy; 2024 "I Can Compile" ahh Editor. All rights reserved.</p>
-      <a href="https://github.com/your-repo" className="inline-flex items-center text-blue-400 hover:text-blue-300 mt-2">
+{/*       <a href="https://github.com/your-repo" className="inline-flex items-center text-blue-400 hover:text-blue-300 mt-2">
         View on GitHub
-      </a>
+      </a> */}
+
+      <div className='dark:text-black text-gray-200 flex justify-around sm:w-[250px] w-full'>
+                    <a
+                        href="mailto:yatharthpatel014@gmail.com"
+                        rel="noreferrer"
+                        className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1'
+                        style={{ cursor: 'pointer' }}
+                    >
+                        <FiMail className='text-xl' />
+                    </a>
+                    <a
+                        href="https://x.com/ptlyatharth"
+                        target='_blank'
+                        rel="noreferrer"
+                        className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1'
+                        style={{ cursor: 'pointer' }}
+                    >
+                        <FaTwitter className='text-xl' />
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/yatharth-patel-2505b4288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+                        target='_blank'
+                        rel="noreferrer"
+                        className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1'
+                        style={{ cursor: 'pointer' }}
+                    >
+                        <AiFillLinkedin className='text-xl' />
+                    </a>
+                    <a
+                        href="https://github.com/y4th4rthh"
+                        target='_blank'
+                        rel="noreferrer"
+                        className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1'
+                        style={{ cursor: 'pointer' }}
+                    >
+                        <FaGithub className='text-xl' />
+                    </a>
+                </div>
     </div>
   </footer>
 );
